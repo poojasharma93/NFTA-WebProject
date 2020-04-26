03452ed5abcaf4b594a2272ff8500a287d35911c
aditya0335 <bansaladitya0335@gmail.com>
2020-04-26 03:03:34 -0400

    Table structure change for transaction and service request

--- src/TransactionResolved.js
+++ src/TransactionResolved.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import FilterForm from "./FilterForm";
 import Cookies from "universal-cookie";
 import { Redirect } from "react-router";
+import BootstrapTable from "react-bootstrap-table-next";
+import paginationFactory from "react-bootstrap-table2-paginator";
+import "react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css";
+import "react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit.min.css";
+import filterFactory, { textFilter } from "react-bootstrap-table2-filter";
 
 const cookies = new Cookies();
 class TransactionResolved extends Component {
@@ -86,6 +91,109 @@ class TransactionResolved extends Component {
       );
   }
 
+  transactionFilter;
+  stopidFilter;
+  directionFilter;
+  countyFilter;
+  requestFilter;
+  caret = (order, column) => {
+    if (!order) return <span>&nbsp;&nbsp;Desc/Asc</span>;
+    else if (order === "asc")
+      return (
+        <span>
+          &nbsp;&nbsp;Desc/<font color="red">Asc</font>
+        </span>
+      );
+    else if (order === "desc")
+      return (
+        <span>
+          &nbsp;&nbsp;<font color="red">Desc</font>/Asc
+        </span>
+      );
+    return null;
+  };
+
+  columns = [
+    {
+      dataField: "transaction_no",
+      text: "TransactionID",
+      id: "transaction_no",
+      sort: true,
+      filter: textFilter({
+        getFilter: filter => {
+          this.transactionFilter = filter;
+        }
+      }),
+      sortCaret: this.caret
+    },
+    {
+      dataField: "stop_id",
+      text: "StopID",
+      sort: true,
+      filter: textFilter({
+        getFilter: filter => {
+          this.stopidFilter = filter;
+        }
+      }),
+      sortCaret: this.caret
+    },
+    {
+      dataField: "direction.display_name",
+      text: "Direction",
+      sort: true,
+      filter: textFilter({
+        getFilter: filter => {
+          this.directionFilter = filter;
+        }
+      }),
+      sortCaret: this.caret
+    },
+    {
+      dataField: "county.display_name",
+      text: "County",
+      sort: true,
+      filter: textFilter({
+        getFilter: filter => {
+          this.countyFilter = filter;
+        }
+      }),
+      sortCaret: this.caret
+    },
+    {
+      dataField: "work_request.request_id",
+      text: "Request ID",
+      sort: true,
+      filter: textFilter({
+        getFilter: filter => {
+          this.requestFilter = filter;
+        }
+      }),
+      sortCaret: this.caret
+    },
+    {
+      dataField: "transaction_no",
+      formatter: (cell, row) => {
+        return (
+          <p>
+            <a href={"/requestStatusDetail/" + row.transaction_no}>
+              View Details
+            </a>
+          </p>
+        );
+      },
+      text: "View Details",
+      sort: false,
+      style: { color: "blue" }
+    }
+  ];
+  handleClick = () => {
+    this.transactionFilter("");
+    this.stopidFilter("");
+    this.directionFilter("");
+    this.countyFilter("");
+    this.requestFilter("");
+  };
+
   render() {
     if (this.state.redirect) {
       return (
@@ -104,37 +212,29 @@ class TransactionResolved extends Component {
       <div>
         <FilterForm handleOnClick={this.handleOnClick} />
 
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">TransactionID</th>
-              <th scope="col">StopID</th>
-              <th scope="col">Direction</th>
-              <th scope="col">County</th>
-              <th scope="col">Request ID</th>
-              <th></th>
-            </tr>
-            {transactions.map(trans => (
-              <tr key={trans.transaction_no}>
-                <td> {trans.transaction_no} </td>
-                <td> {trans.stop_id}</td>
-                <td>{trans.direction ? trans.direction.display_name : ""}</td>
-                <td>{trans.county ? trans.county.display_name : ""}</td>
-                <td>
-                  {trans.work_request ? trans.work_request.request_id : ""}
-                </td>
-                <td>
-                  <a
-                    href={`/requestStatusDetail/${trans.transaction_no}`}
-                    id="link"
-                  >
-                    View Details
-                  </a>
-                </td>
-              </tr>
-            ))}
-          </thead>
-        </table>
+        <hr />
+        <button
+          className="btn btn-lg btn-primary align-bottom"
+          onClick={this.handleClick}
+        >
+          {/* {" "} */}
+          Clear all filters
+          {/* {" "} */}
+        </button>
+        <BootstrapTable
+          keyField="transaction_no"
+          data={transactions}
+          columns={this.columns}
+          hover="true"
+          filter={filterFactory()}
+          pagination={paginationFactory({
+            sizePerPage: 10,
+            hideSizePerPage: true,
+            showTotal: true,
+            withFirstAndLast: true,
+            alwaysShowAllBtns: true
+          })}
+        />
       </div>
     );
   }
